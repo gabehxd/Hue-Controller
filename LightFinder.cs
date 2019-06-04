8495489e7a61118a7aa590edc9ccbd270ec42039
Sun <22406854+SunTheCourier@users.noreply.github.com>
2019-06-03 22:19:36 -0700

    Update LightFinder.cs

--- LightFinder.cs
+++ LightFinder.cs
@@ -3,13 +3,8 @@ using Q42.HueApi;
 using Q42.HueApi.Interfaces;
 using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Hue_Controller
